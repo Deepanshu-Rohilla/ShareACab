8a16a16674f72d7f718d911f89dd64491c68a27b
Deepanshu Rohilla <deepanshu38841@gmail.com>
2020-06-07 02:02:52 +0530

    implemented hero animations

--- lib/screens/groupdetailscreen/groupdetails.dart
+++ lib/screens/groupdetailscreen/groupdetails.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/scheduler.dart';
 //import './appbar.dart';
 import 'package:shareacab/services/trips.dart';
 import 'package:provider/provider.dart';
@@ -14,8 +15,11 @@ class GroupDetails extends StatelessWidget {
   final privacy;
   final start;
   final end;
+  final numberOfMembers;
+  final data;
 
-  GroupDetails(this.destination, this.start, this.end, this.docId, this.privacy);
+  GroupDetails(this.destination, this.docId, this.privacy, this.start, this
+      .end, this.numberOfMembers , this.data);
 
   final RequestService _request = RequestService();
 
@@ -36,173 +40,191 @@ class GroupDetails extends StatelessWidget {
         inGroup = false;
       }
     });
+    timeDilation = 2.0;
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Group Details'),
+      ),
       body: FutureBuilder(
           future: getUserDetails(),
-          builder: (ctx, futureSnapshot) {
+          builder: (ctx, futureSnapshot){
             if (futureSnapshot.connectionState == ConnectionState.waiting) {
               return Center(
                 child: CircularProgressIndicator(),
               );
             }
-            return CustomScrollView(
-              slivers: <Widget>[
-                SliverAppBar(
-                  pinned: true,
-                  floating: false,
-                  //expandedHeight: 210,
-                  expandedHeight: 120,
-                  flexibleSpace: FlexibleSpaceBar(
-                    // background: Image.asset(
-                    //   destination == 'New Delhi Railway Station' ? 'assets/images/train.jpg' : 'assets/images/plane.jpg',
-                    //   fit: BoxFit.cover,
-                    // ),
-                    //title: AppBarTitle(destination),
-                    // THE ABOVE WAS THROWING AN ERROR, WILL CHECK LATER
-                    title: Text(destination),
-                  ),
-                ),
-                SliverList(
-                  delegate: SliverChildListDelegate(
-                    [
-                      Column(
-                        children: <Widget>[
-                          Container(
-                            margin: EdgeInsets.symmetric(vertical: 13, horizontal: 10),
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: Theme.of(context).accentColor,
-                                  width: 0.25,
-                                ),
-                                borderRadius: BorderRadius.circular(15)),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
+            return SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  Hero(
+                    tag: docId,
+                    child: Card(
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(25.0))),
+                      elevation: 5,
+                      margin: EdgeInsets.symmetric(vertical: 6, horizontal: 5),
+                      child: Container(
+                        height: 150,
+                        child: Column(
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: <Widget>[
-                                Container(
-                                  padding: EdgeInsets.only(
-                                    top: 10,
-                                    bottom: 10,
-                                    left: 10,
-                                    right: 50,
-                                  ),
-                                  decoration: BoxDecoration(
-                                    border: Border(
-                                      right: BorderSide(
-                                        color: Theme.of(context).accentColor,
-                                        width: 0.25,
+                                Flexible(
+                                  fit: FlexFit.tight,
+                                  flex: 1,
+                                  child: Container(
+                                      margin: EdgeInsets.only(
+                                        left: 20,
+                                        top: 20,
                                       ),
+                                      child: destination == 'New Delhi Railway Station'
+                                          ? Icon(
+                                        Icons.train,
+                                        color: Theme.of(context).accentColor,
+                                        size: 30,
+                                      )
+                                          : Icon(
+                                        Icons.airplanemode_active,
+                                        color: Theme.of(context).accentColor,
+                                        size: 30,
+                                      )),
+                                ),
+                                Flexible(
+                                  fit: FlexFit.tight,
+                                  flex: 4,
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(top: 10.0),
+                                    child: Text(
+                                      '${destination}',
+                                      style: TextStyle(
+                                        fontSize: 17,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      textAlign: TextAlign.center,
                                     ),
                                   ),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Text(
-                                          'Start:',
-                                          style: TextStyle(letterSpacing: 2),
-                                        ),
+                                ),
+                                Flexible(
+                                  flex: 2,
+                                  child: Container(
+                                    child: privacy == 'true'
+                                        ? Padding(
+                                      padding: const EdgeInsets.only(right: 15.0),
+                                      child: Icon(
+                                        Icons.lock,
+                                        color: Theme.of(context).accentColor,
                                       ),
-                                      Text(
-                                        DateFormat('dd.MM.yyyy - kk:mm a').format(start),
-                                        style: TextStyle(letterSpacing: 2),
-                                      ),
-                                    ],
+                                    )
+                                        : !inGroup
+                                        ? FlatButton(
+                                      onPressed: () async {
+                                        try {
+                                          DocumentSnapshot temp = data;
+                                          await _request.joinGroup(temp.documentID);
+                                          await Navigator.of(context).pop();
+                                        } catch (e) {
+                                          print(e.toString());
+                                        }
+                                      },
+                                      child: Text('Join Now'),
+                                    )
+                                        : FlatButton(
+                                      onPressed: null,
+                                      child: Text('Already in group'),
+                                    ),
                                   ),
+                                )
+                              ],
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                bottom: 5,
+                                top: 10,
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text(
+                                    'Start : ${DateFormat('dd.MM.yyyy - kk:mm a').format
+                                      (start)}',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(
+                                bottom: 5,
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Text(
+                                    'End : ${DateFormat('dd.MM.yyyy - kk:mm a').format(end
+                                    )}',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: <Widget>[
+                                Column(
+                                  children: <Widget>[Text('Number of members in group: '
+                                      '${numberOfMembers}')],
                                 ),
                               ],
                             ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                                  color: Theme.of(context).accentColor,
-                                  width: 0.25,
-                                ),
-                                borderRadius: BorderRadius.circular(15)),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Container(
-                                  padding: EdgeInsets.only(
-                                    top: 10,
-                                    bottom: 10,
-                                    left: 10,
-                                    right: 50,
-                                  ),
-                                  decoration: BoxDecoration(
-                                    border: Border(
-                                      right: BorderSide(
-                                        color: Theme.of(context).accentColor,
-                                        width: 0.25,
-                                      ),
-                                    ),
-                                  ),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      Padding(
-                                        padding: const EdgeInsets.all(8.0),
-                                        child: Text(
-                                          'End:',
-                                          style: TextStyle(letterSpacing: 2),
-                                        ),
-                                      ),
-                                      Text(
-                                        DateFormat('dd.MM.yyyy - kk:mm a').format(end),
-                                        style: TextStyle(
-                                          letterSpacing: 2,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SingleChildScrollView(
-                            child: Container(
-                              margin: EdgeInsets.only(top: 60),
-                              height: MediaQuery.of(context).size.height * 0.7,
-                              child: ListView.builder(
-                                  itemCount: futureSnapshot.data.length,
-                                  itemBuilder: (ctx, index) {
-                                    return Container(
-                                      margin: EdgeInsets.symmetric(vertical: 2, horizontal: 10),
-                                      width: double.infinity,
-                                      child: Card(
-                                        elevation: 4,
-                                        child: Row(
-                                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                          children: <Widget>[
-                                            Padding(
-                                              padding: const EdgeInsets.all(8.0),
-                                              child: Text(futureSnapshot.data[index].data['name']),
-                                            ),
-                                            Padding(
-                                              padding: const EdgeInsets.all(8.0),
-                                              child: Text(futureSnapshot.data[index].data['hostel']),
-                                            ),
-                                            Padding(
-                                              padding: const EdgeInsets.all(8.0),
-                                              child: IconButton(onPressed: () {}, icon: Icon(Icons.phone)),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    );
-                                  }),
-                            ),
-                          )
-                        ],
-                      )
-                    ],
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ],
+                  SingleChildScrollView(
+                    child: Container(
+                      margin: EdgeInsets.only(top: 60),
+                      height: MediaQuery.of(context).size.height * 0.7,
+                      child: ListView.builder(
+                          itemCount: futureSnapshot.data.length,
+                          itemBuilder: (ctx, index) {
+                            return Container(
+                              margin: EdgeInsets.symmetric(vertical: 2, horizontal: 10),
+                              width: double.infinity,
+                              child: Card(
+                                elevation: 4,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(futureSnapshot.data[index].data['name']),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(futureSnapshot.data[index].data['hostel']),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: IconButton(onPressed: () {}, icon: Icon(Icons.phone)),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          }),
+                    ),
+                  )
+                ],
+              ),
             );
-          }),
+          }
+      ),
       bottomNavigationBar: FlatButton(
         onPressed: () async {
           try {
@@ -219,17 +241,19 @@ class GroupDetails extends StatelessWidget {
         padding: EdgeInsets.all(20),
         child: privacy == 'true'
             ? Text(
-                'Request to Join',
-                style: TextStyle(fontSize: 20),
-              )
+          'Request to Join',
+          style: TextStyle(fontSize: 20),
+        )
             : inGroup
-                ? Text(
-                    'Already in a Group',
-                    style: TextStyle(fontSize: 20),
-                  )
-                : Text('Join Now', style: TextStyle(fontSize: 20)),
+            ? Text(
+          'Already in a Group',
+          style: TextStyle(fontSize: 20),
+        )
+            : Text('Join Now', style: TextStyle(fontSize: 20)),
         color: Theme.of(context).accentColor,
       ),
+
     );
   }
 }
+//
